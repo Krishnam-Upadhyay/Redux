1151cddc81241936ed113472e11286bc704e9a58
Krishnam-Upadhyay <upadhyaykrishnam21@gmail.com>
2023-07-20 11:43:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Redux toolkit</h1>
+      <p>Ny Krishnam UPadhayy</p>
+    </div>
+  );
+}
