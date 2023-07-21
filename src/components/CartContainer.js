6736f12609a13d3e0c605e88818bc45c26edec6d
Krishnam-Upadhyay <upadhyaykrishnam21@gmail.com>
2023-07-21 19:11:24 +0530

    added the button functions

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,31 +1,44 @@
 import CartItem from './CartItem';
 import React from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearCart, removeItem } from '../features/cart/cartSlice';
 
 const CartContainer = () => {
-  const {cartItem,amount,total} = useSelector((store)=>store.cart);
+  const dispatch = useDispatch();
+  const { cartItem, amount, total } = useSelector((store) => store.cart);
 
-  if(amount<1){
-    return <section className = 'cart'>
-      <header>
-        <h2> Your bag </h2>
-        <h4 className = 'empty-cart'>Your car is empty</h4>
-      </header>
-
-
-    </section>
-    
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2> Your bag </h2>
+          <h4 className="empty-cart">Your car is empty</h4>
+        </header>
+      </section>
+    );
   }
-  return <section className = 'cart'>
-  <header>
-    <h2> Your bag </h2>
-    
-  </header>
-  <div>{cartItem.map((item)=>{
-    return <CartItem key = {item.id} {...item} />
-  })}</div>
-
-
-</section>;
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItem.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
+      </footer>
+    </section>
+  );
 };
 export default CartContainer;
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from './../../components//cartItems';
 
 const initialState = {
   cartItem: cartItems,
-  amount: 2,
+  amount: 5,
   total: 0,
   isLoading: true,
 };
@@ -11,7 +11,31 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItem = state.cartItem.filter((item) => {
+        return item.id !== itemId;
+      });
+    },
+    increase: (state, { payload }) => {
+      const Item = state.cartItem.find((item) => {
+        return item.id === payload.id;
+      });
+      Item.amount = Item.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const Item = state.cartItem.find((item) => {
+        return item.id === payload.id;
+      });
+      Item.amount = Item.amount - 1;
+    },
+  },
 });
 
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 // console.log(cartSlice);
 export default cartSlice.reducer;
